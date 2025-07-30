5f98c12281b87a70aa3a2630c902247fb8dbbd5b
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2025-07-30 10:13:52 +0400

    implemented websocket

--- src/main/java/org/example/adapterwebsocket/client/CryptoCurrencyClient.java
+++ src/main/java/org/example/adapterwebsocket/client/CryptoCurrencyClient.java
@@ -3,11 +3,12 @@ package org.example.adapterwebsocket.client;
 import org.example.adapterwebsocket.client.model.RateStreamControlRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "crypto-currency", url = "localhost:8099/v1/crypto-currencies")
 public interface CryptoCurrencyClient {
 
     @PostMapping("/rate/streaming")
-    void controlRateStreaming(RateStreamControlRequest request);
+    void controlRateStreaming(@RequestBody RateStreamControlRequest request);
 
 }
