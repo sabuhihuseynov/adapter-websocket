d7a36b59af86a17b3e83768ce2480c7a5ad0d8a7
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2025-08-06 09:44:18 +0400

    changed naming for entity

--- src/main/java/org/example/adapterwebsocket/controller/CryptoRateSubscriptionRestController.java
+++ src/main/java/org/example/adapterwebsocket/controller/CryptoRateSubscriptionRestController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.adapterwebsocket.model.CryptoRateSubscriptionCheckResponse;
 import org.example.adapterwebsocket.model.CurrencyPair;
 import org.example.adapterwebsocket.service.CryptoRateSubscriptionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,9 @@ public class CryptoRateSubscriptionRestController {
     private final CryptoRateSubscriptionService subscriptionService;
 
     @GetMapping("/check")
-    public CryptoRateSubscriptionCheckResponse hasPairSubscription(CurrencyPair currencyPair) {
+    public ResponseEntity<CryptoRateSubscriptionCheckResponse> hasPairSubscription(CurrencyPair currencyPair) {
         boolean hasActiveSubscriptions = subscriptionService.hasPairSubscription(currencyPair);
         log.info("Subscription check for {}: {}", currencyPair, hasActiveSubscriptions);
-        return new CryptoRateSubscriptionCheckResponse(hasActiveSubscriptions);
+        return ResponseEntity.ok(new CryptoRateSubscriptionCheckResponse(hasActiveSubscriptions));
     }
 }
